17d76ab00a212e6cd66f96fac02b6e5ed7143c3a
smelio95 <stephanemawanzi@gmail.com>
2026-02-19 19:21:45 +0100

    fix prisma not defined in getEmployees

--- backend/src/user/user.controller.js
+++ backend/src/user/user.controller.js
@@ -1,3 +1,4 @@
+import prisma from '../prisma.js';
 import { createUser, loginUser, getUserById, updateUser, deleteUser as deleteUserFromDB } from './user.model.js';
 
 // Inscription d'un nouvel utilisateur
